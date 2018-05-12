4ed63fce689cf0547441bf15d866081bdc068958
wade <620781804@qq.com>
2018-05-12 14:25:41 +0800

    wade

--- src/breadNav/home.vue
+++ src/breadNav/home.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    我是 HOME
+        <div style="borderRadius:10px" class="carouselcontent">
+            <el-carousel :interval="5000"  height="400px" indicator-position="none">
+              <el-carousel-item v-for="item in 4" :key="item">
+                <h3><img src='../assets/mm'+item+'.jpg' alt="">
+                
+                </h3>
+              </el-carousel-item>
+            </el-carousel>
+        </div>
   </div>
 </template>
 
